2b924d1f1dd1f3cdfdf68849a41346c3704b68c9
JananaSS05 <97337723+JananaSS05@users.noreply.github.com>
2022-07-08 12:31:30 +0300

    sdd

--- Assets/Scripts/bird_animation.cs
+++ Assets/Scripts/bird_animation.cs
@@ -7,6 +7,7 @@ public class bird_animation : MonoBehaviour
     public Sprite[] sprites;
     public int framesPerSecond;
     public float velocity;
+    public bool isRotate;
     public Rigidbody2D rb;
     public GameManager gameManager;
 
@@ -32,16 +33,19 @@ public class bird_animation : MonoBehaviour
             rb.velocity = Vector2.up * velocity;
         }
 
-        float rotation = rb.velocity.y * 0.15f;
+        if(isRotate == true)
+        {
+            float rotation = rb.velocity.y * 0.15f;
 
-        if (transform.rotation.z > 0.18f && rotation > 0) rotation = 0;
-        else if (transform.rotation.z < -0.4f && rotation < 0) rotation = 0;
+            if (transform.rotation.z > 0.18f && rotation > 0) rotation = 0;
+            else if (transform.rotation.z < -0.4f && rotation < 0) rotation = 0;
 
-        if (transform.rotation.eulerAngles.z < -60)
-            rotation = -60;
+            if (transform.rotation.eulerAngles.z < -60)
+                rotation = -60;
 
-        transform.Rotate(0, 0, rotation, Space.Self);
-
+            transform.Rotate(0, 0, rotation, Space.Self);
+        }
+        
         
 
         if (timer >= 1.0f / framesPerSecond)
